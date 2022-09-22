527e6146bd69be50e626286b31424c869167c2b3
Tim <timwagner997@gmail.com>
2022-09-22 22:54:28 +0200

    Anschreiben fertiggestellt

--- lib/screens/letter.dart
+++ lib/screens/letter.dart
@@ -5,9 +5,56 @@ class Letter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool letterDoesNotFitA4 = MediaQuery.of(context).size.width < 430;
+
+    Widget letterCard = Card(
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Sehr geehrte Damen und Herren,\n\n\n"
+              "Lorem ipsum dolor sit amet, "
+              "consetetur sadipscing elitr, sed diam nonumy eirmod tempor "
+              "invidunt ut labore et dolore magna aliquyam erat, sed diam "
+              "voluptua. At vero eos et accusam et justo duo dolores et ea rebum. "
+              "Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
+              "ipsum dolor sit amet.\n\nLorem ipsum dolor sit amet, consetetur "
+              "sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut "
+              "labore et dolore magna aliquyam erat, sed diam voluptua. "
+              "At vero eos et accusam et justo duo dolores et ea rebum. "
+              "Stet clita kasd gubergren, no sea takimata sanctus est Lorem "
+              "ipsum dolor sit amet.\n\n\n"
+              "Mit freundlichen Grüßen",
+              textAlign: TextAlign.justify,
+            ),
+            Image.asset(
+              "images/signature.png",
+              width: 200,
+            ),
+            const Text("Max Mustermann")
+          ],
+        ),
+      ),
+    );
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Anschreiben"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: SingleChildScrollView(
+          child: Center(
+            child: SizedBox(
+              width: 500,
+              child: letterDoesNotFitA4
+                  ? letterCard
+                  : AspectRatio(aspectRatio: 1 / 1.4142, child: letterCard),
+            ),
+          ),
+        ),
       ),
     );
   }
